ec5e6a0c429171ecfce4a157722f80c4757fbce6
tiago.rocha@iaudit.com.br <Pa$$w0rd2020@!>
2021-05-05 14:25:40 -0300

    Layers

---
+++ backend/src/main/java/com/devsuperior/dsvendas/repositores/SallerRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dsvendas.repositores;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dsvendas.entities.Seller;
+
+public interface SallerRepository extends JpaRepository<Seller, Long>{
+
+}
