e7c3a2d250124c236443ae0878a0a148535c67c5
Andre Pereira <andrelpti@gmail.com>
2021-05-06 18:31:03 -0300

    mudança layout cor

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,9 +1,9 @@
 const Footer = () => {
     return (
-        <footer className="footer mt-auto py-3 bg-dark">
+        <footer className="footer mt-auto py-3">
             <div className="container">
-                <p className="text-light">App desenvolvido por <a href="https://github.com/andrelpti" target="_blank" rel="noreferrer">Andre Pereira</a></p>
-                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                <p className="text-primary">App desenvolvido por <a href="https://github.com/andrelpti" target="_blank" rel="noreferrer">Andre Pereira</a></p>
+                <p className="text-primary"><small><strong>Semana Spring React</strong><br />
       Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
             </div>
         </footer>
