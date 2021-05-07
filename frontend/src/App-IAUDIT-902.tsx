aa9488776204ad225de43ab53b4f9596b42a3c92
tiago.rocha@iaudit.com.br <Pa$$w0rd2020@!>
2021-05-07 09:09:23 -0300

    Routes

---
+++ frontend/src/App-IAUDIT-902.tsx
@@ -0,0 +1,38 @@
+import BarChart from "components/BarChart";
+import DataTable from "components/DataTable";
+import DonutChart from "components/DonutChart";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
+function App() {
+  return (
+    <>
+      <div className="bg-dark">
+        <NavBar />
+        <div className="container">
+          <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+
+          <div className="row px-3 bg-white">
+            <div className="col-sm-6">
+              <h5 className="text-center text-primary"> Taxa de sucesso (%)</h5>
+              <BarChart />
+            </div>
+            <div className="col-sm-6">
+              <h5 className="text-center text-primary"> Total de Vendas</h5>
+              <DonutChart />
+            </div>
+          </div>
+
+          <div className="py3">
+            <h2 className="text-primary">Todas Vendas</h2>
+          </div>
+
+          <DataTable />
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Dashboard/index.tsx
@@ -0,0 +1,38 @@
+import BarChart from "components/BarChart";
+import DataTable from "components/DataTable";
+import DonutChart from "components/DonutChart";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
+const Dashboard = () => {
+    return (
+        <>
+            <div className="bg-dark">
+                <NavBar />
+                <div className="container">
+                    <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+
+                    <div className="row px-4 bg-white">
+                        <div className="col-sm-6">
+                            <h5 className="text-center text-primary"> Taxa de sucesso (%)</h5>
+                            <BarChart />
+                        </div>
+                        <div className="col-sm-6">
+                            <h5 className="text-center text-primary"> Total de Vendas</h5>
+                            <DonutChart />
+                        </div>
+                    </div>
+
+                    <div className="py-3">
+                        <h1 className="text-primary">Todas Vendas</h1>
+                    </div>
+
+                    <DataTable />
+                </div>
+            </div>
+            <Footer />
+        </>
+    );
+}
+
+export default Dashboard;
